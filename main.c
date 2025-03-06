dd66a27a39aa85d59791adbabf783febdb92b270
patschaef <schaefp1@tcnj.edu>
2025-03-06 15:18:18 -0500

    Created main with some pseudocomments and built a makefile

---
+++ main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+
+//thread algorithms to validate
+void column(void* param);
+void row(void* param);
+void square(void* param);
+
+//gloabl vairable to fill with input
+int puzzle[9][9];
+
+int main(int argc, char** argv){
+	//Open and read file form argv[1]
+
+	//Create threads for rows
+	//Create threads for columns
+	//Create threads for squares
+
+
+	//Join Threads
+
+	return 0;
+}
