4e5023152f92f593cc916a44e3271e35254f76c6
patschaef <schaefp1@tcnj.edu>
2025-03-08 13:42:45 -0500

    input.txt is read and its contents are loaded into puzzle 2D array

--- main.c
+++ main.c
@@ -11,7 +11,20 @@ void square(void* param);
 int puzzle[9][9];
 
 int main(int argc, char** argv){
-	//Open and read file form argv[1]
+	int option = 0;
+	FILE *fptr = NULL;
+
+	if(argc != 2){
+		fprintf(stderr, "Bad Arguments, please select 1 option\n");
+	}
+	//Open and read input.txt
+	fptr = fopen("input.txt", "r");
+	if(fptr == NULL)fprintf(stderr, "Error opening input.txt\n");
+	for(int i = 0; i < 9; i++){
+		for (int j = 0; j < 9; j++){
+			fscanf(fptr, "%d ", &puzzle[i][j]);
+		}
+	}	
 
 	//Create threads for rows
 	//Create threads for columns
@@ -20,5 +33,7 @@ int main(int argc, char** argv){
 
 	//Join Threads
 
+
+	fclose(fptr);
 	return 0;
 }
