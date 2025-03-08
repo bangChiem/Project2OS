a422e38dc960cc1cf4847eb34d7ec10769a0560a
Patrick Schaeffer <151101583+patschaef@users.noreply.github.com>
2025-03-08 14:02:39 -0500

    Fix branch issue

--- main.c
+++ main.c
@@ -50,10 +50,21 @@ int puzzle[9][9];
 int correct_puzzle = 1;
 
 int main(int argc, char** argv){
+	int option = 0;
+	FILE *fptr = NULL;
 
-
-	//Open and read file form argv[1]
-
+	if(argc != 2){
+		fprintf(stderr, "Bad Arguments, please select 1 option\n");
+	}
+	//Open and read input.txt
+	fptr = fopen("input.txt", "r");
+	if(fptr == NULL)fprintf(stderr, "Error opening input.txt\n");
+	for(int i = 0; i < 9; i++){
+		for (int j = 0; j < 9; j++){
+			fscanf(fptr, "%d ", &puzzle[i][j]);
+		}
+	}	
+	
 	//Create threads for rows
 	//Create threads for columns
 	//Create threads for squares
