80e99d7d49e8988e1574da57311a143f486b6c31
Bang <bangchiem40@gmail.com>
2025-03-08 16:11:35 -0500

    merged square and row col functions

--- main.c
+++ main.c
@@ -7,14 +7,36 @@
 int puzzle[9][9];
 
 //global flag variable to declare if puzzle is correct or wrong
-int correct_puzzle = 1;
+int correctPuzzle = 1;
+
+void * square(void* param){
+	int squareNum = *(int*)param;
+	int checker[9] = {0};
+ 
+
+	//Load in the square
+	for(int i = 0; i<3; i++){
+		for(int j = 0; j<3; j++){
+			int pRow = squareNum/3 *3 + i;
+			int pCol = squareNum%3 *3 + j;
+			int val = puzzle[pRow][pCol];
+			checker[val]++;
+		}
+	}
+
+	//validates puzzle
+	for(int i = 0; i < 9; i++){
+		if(correctPuzzle != 1){
+			correctPuzzle = checker[i] == 1;
+		}		
+	}
+	pthread_exit(0);
+}
+
 
 //thread algorithms to validate
-void column(void* param){
+void *column(void* param){
 
-	// used to keep track of if digit 1-9 is in column
-	// ex. if checker[0] is 1 then 1 is in column...
-	// if checker[3] is 0 then 4 is NOT in column
 	int checker[9];
 
 	// initialize checker arr to all 0
@@ -24,10 +46,13 @@ void column(void* param){
 	}
 
 	// go each val through each val in column and fill in checker array
-	int column = *(int*)param;
+	int column = *(int*)param - 1;
 	int temp;
 	for (int i = 0; i < 9; i++){
 		temp = puzzle[i][column];
+		if (temp == 0){
+			continue;;
+		}
 		checker[temp - 1] = 1;
 	}
 
@@ -36,27 +61,83 @@ void column(void* param){
 	{
 		if (checker[i] == 0){
 			// mark global validator value false
-			correct_puzzle = 0;
+			correctPuzzle = 0;
 		}
 	}
-
-}
-void row(void* param){
-	int row = *(int*)param;
 }
 
-void square(void* param){
-	int square = *(int*)param;
+void *row(void* param){
+	int checker[9];
+
+	for (int i = 0; i < 9; i++)	// initialize checker arr to all 0
+	{
+		checker[i] = 0;
+	}
+
+	// go each val through each val in row and fill in checker array
+	int row = *(int*)param - 1;
+	int temp;
+	for (int i = 0; i < 9; i++){
+		temp = puzzle[row][i];
+		if (temp == 0){
+			continue;;
+		}
+		checker[temp - 1] = 1;
+	}
+
+	// if all values in checker are 1 then column has all digits 1-9
+	for (int i = 0; i < 9; i++)
+	{
+		if (checker[i] == 0){
+			// mark global validator value false
+			correctPuzzle = 0;
+		}
+	}
+}
+ 
+void call_threads(int option){
+	pthread_t pid[9];
+	pthread_t square_params[9];
+	for(int i = 0; i < 9; i++){
+		square_params[i] = i;
+		pthread_create(&pid[i], NULL, square, &square_params[i]);
+	}
+
+	for(int i = 0; i < 9; i++){
+		pthread_join(pid[i], NULL);
+	}
+
+	pthread_t rows_t[9];
+	pthread_t cols_t[9];
+
+	// create threads to cols
+	int cols[9];
+	int rows[9];
+	for (int i = 0; i < 9; i++)
+	{
+		cols[i] = i + 1;
+		rows[i] = i + 1;
+		pthread_create(&rows_t[i], NULL, row, &rows[i]);
+		pthread_create(&cols_t[i], NULL, column, &cols[i]);
+	}
+
+	// join threads
+	for (int i = 0; i < 9; i++)
+	{
+		pthread_join(rows_t[i], NULL);
+		pthread_join(cols_t[i], NULL);
+	}
 }
 
 int main(int argc, char** argv){
-	time_t begin = time(NULL);
+	clock_t begin = clock();
 	int option = 0;
 	FILE *fptr = NULL;
 
 	if(argc != 2){
 		fprintf(stderr, "Bad Arguments, please select 1 option\n");
 	}
+
 	//Open and read input.txt
 	fptr = fopen("input.txt", "r");
 	if(fptr == NULL)fprintf(stderr, "Error opening input.txt\n");
@@ -68,7 +149,27 @@ int main(int argc, char** argv){
 		printf("\n");
 	}
 
-	printf("SOLUTION: *** in %ld", time(NULL) - begin);
+	// get commandline arg
+	int version = atoi(argv[1]);
+
+	// create pids
+	int num_threads;
+	if (version == 2){
+		num_threads = 9;
+	}
+
+	char* SOLUTION;
+	clock_t end = clock();
+
+	call_threads(2);
+
+	double duration = (double) (end - begin) / CLOCKS_PER_SEC;
+	if (correctPuzzle){
+		printf("SOLUTION: YES in %f\n", duration);
+	}
+	else{
+		printf("SOLUTION: NO in %f\n", duration);
+	}
 	
 	
 	//Create threads for rows
